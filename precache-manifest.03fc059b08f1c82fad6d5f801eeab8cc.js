08ece484665e86397c0b4850783d2b7a3a402ef6
barbac <jjbarbac+git@gmail.com>
2019-06-23 15:17:05 -0500

    first

---
+++ precache-manifest.03fc059b08f1c82fad6d5f801eeab8cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d6ea83139c3bc69f28fab6aef6a6ae",
+    "url": "/parasvg/index.html"
+  },
+  {
+    "revision": "cd81fb4acddfba6cdc9b",
+    "url": "/parasvg/static/css/main.a755640d.chunk.css"
+  },
+  {
+    "revision": "0e1304d5278ec4ddc757",
+    "url": "/parasvg/static/js/2.5c374fe7.chunk.js"
+  },
+  {
+    "revision": "cd81fb4acddfba6cdc9b",
+    "url": "/parasvg/static/js/main.9ce83354.chunk.js"
+  },
+  {
+    "revision": "c425f635fb3305a3db5f",
+    "url": "/parasvg/static/js/runtime~main.0e1876a2.js"
+  }
+]);
